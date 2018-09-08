290d10819863f6e18423d4293b5ed760e5d29061
Sergey Vinokurov <serg.foo@gmail.com>
2018-09-08 08:40:19 +0100

    Only update loaded modules if anything changed

--- src/Control/Monad/Filesystem.hs
+++ src/Control/Monad/Filesystem.hs
@@ -18,6 +18,8 @@ import Control.Monad.Except
 import Control.Monad.Reader
 import qualified Control.Monad.State.Lazy as Lazy
 import qualified Control.Monad.State.Strict as Strict
+import Control.Monad.Writer as Lazy
+import Control.Monad.Writer.Strict as Strict
 import qualified Data.Text as T
 import qualified Data.Text.Lazy as TL
 import qualified Data.Text.Lazy.IO
@@ -59,6 +61,30 @@ instance MonadFS m => MonadFS (ExceptT e m) where
   getDirectoryContents = lift . getDirectoryContents
 
 instance MonadFS m => MonadFS (ReaderT r m) where
+  {-# INLINE getModificationTime  #-}
+  {-# INLINE readFile             #-}
+  {-# INLINE doesFileExist        #-}
+  {-# INLINE doesDirectoryExist   #-}
+  {-# INLINE getDirectoryContents #-}
+  getModificationTime  = lift . getModificationTime
+  readFile             = lift . readFile
+  doesFileExist        = lift . doesFileExist
+  doesDirectoryExist   = lift . doesDirectoryExist
+  getDirectoryContents = lift . getDirectoryContents
+
+instance (MonadFS m, Monoid w) => MonadFS (Lazy.WriterT w m) where
+  {-# INLINE getModificationTime  #-}
+  {-# INLINE readFile             #-}
+  {-# INLINE doesFileExist        #-}
+  {-# INLINE doesDirectoryExist   #-}
+  {-# INLINE getDirectoryContents #-}
+  getModificationTime  = lift . getModificationTime
+  readFile             = lift . readFile
+  doesFileExist        = lift . doesFileExist
+  doesDirectoryExist   = lift . doesDirectoryExist
+  getDirectoryContents = lift . getDirectoryContents
+
+instance (MonadFS m, Monoid w) => MonadFS (Strict.WriterT w m) where
   {-# INLINE getModificationTime  #-}
   {-# INLINE readFile             #-}
   {-# INLINE doesFileExist        #-}
