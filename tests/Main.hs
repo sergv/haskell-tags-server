456e6a084ea820fc90d0e1e0fb0615e936eba669
Sergey Vinokurov <serg.foo@gmail.com>
2018-12-09 20:08:30 +0000

    Disable SIGPIPE when not on Windows

--- tests/Main.hs
+++ tests/Main.hs
@@ -6,11 +6,17 @@
 -- Maintainer  :  serg.foo@gmail.com
 ----------------------------------------------------------------------------
 
+{-# LANGUAGE CPP #-}
+
 module Main (main) where
 
 import Network.Socket (withSocketsDo)
 import System.IO
 import Test.Tasty
+
+#ifndef mingw32_HOST_OS
+import System.Posix (installHandler, sigPIPE, Handler(Ignore))
+#endif
 
 import qualified Data.Map.NonEmpty.Tests as Data.Map.NonEmptyTests
 import qualified Haskell.Language.Lexer.Tests as LexerTests
@@ -21,6 +27,11 @@ import qualified ServerTests
 
 main :: IO ()
 main = withSocketsDo $ do
+
+#ifndef mingw32_HOST_OS
+  _ <- installHandler sigPIPE Ignore Nothing
+#endif
+
   hSetEncoding stdout utf8
   hSetEncoding stderr utf8
   let tests = testGroup "Tests"
