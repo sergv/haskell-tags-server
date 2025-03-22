5b637cccf9caade7b8fb7fd1fc2e672830ca1fc8
Sergey Vinokurov <serg.foo@gmail.com>
2025-03-22 14:21:36 +0000

    Port ‘Haskell.Language.Lexer.Preprocessor.Tests’ to multiline strings

--- tests/Haskell/Language/Lexer/Preprocessor/Tests.hs
+++ tests/Haskell/Language/Lexer/Preprocessor/Tests.hs
@@ -7,6 +7,7 @@
 -- Created     :  29 May 2017
 ----------------------------------------------------------------------------
 
+{-# LANGUAGE MultilineStrings  #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Haskell.Language.Lexer.Preprocessor.Tests (tests) where
@@ -47,72 +48,100 @@ defineTests :: TestTree
 defineTests = testGroup "#define"
   [ testGroup "Constants"
       [ testCase "Vanilla define" $
-          "#define foo bar"
-          ==> PreprocessorConstant (mkConstantMacroDef "foo" "bar")
+        """
+        #define foo bar
+        """
+        ==> PreprocessorConstant (mkConstantMacroDef "foo" "bar")
       , testCase "Define with some spaces" $
-          "# define FOO qu ux"
-          ==> PreprocessorConstant (mkConstantMacroDef "FOO" "qu ux")
+        """
+        # define FOO qu ux
+        """
+        ==> PreprocessorConstant (mkConstantMacroDef "FOO" "qu ux")
       , testCase "Define with lots of continuation lines" $
-          "# \\\n\
-          \ define \\\n\
-          \ FOO \\\n\
-          \ qu \\\n\
-          \ ux"
-          ==> PreprocessorConstant (mkConstantMacroDef "FOO" "qu  ux")
+        """
+        # \\
+         define \\
+         FOO \\
+         qu \\
+         ux
+        """
+        ==> PreprocessorConstant (mkConstantMacroDef "FOO" "qu  ux")
       , testCase "Define with name split by continuation line" $
-          "#define FO\\\n\
-          \O bar"
-          ==> PreprocessorConstant (mkConstantMacroDef "FOO" "bar")
+        """
+        #define FO\\
+        O bar
+        """
+        ==> PreprocessorConstant (mkConstantMacroDef "FOO" "bar")
       , testCase "Malformed define starting with a number" $
-          "#define 1foo bar"
-          !=> Left "defined name > first cpp identifier char: Failed reading: satisfy"
+        """
+        #define 1foo bar
+        """
+        !=> Left "defined name > first cpp identifier char: Failed reading: satisfy"
       , testGroup "Haskell-style names"
           [ testCase "primes and backticks in the body" $
-              "#define foo'_bar` baz"
-              ==> PreprocessorConstant (mkConstantMacroDef "foo'_bar`" "baz")
+            """
+            #define foo'_bar` baz
+            """
+            ==> PreprocessorConstant (mkConstantMacroDef "foo'_bar`" "baz")
           , testCase "prime at the start" $
-              "#define 'foo_bar baz"
-              ==> PreprocessorConstant (mkConstantMacroDef "'foo_bar" "baz")
+            """
+            #define 'foo_bar baz
+            """
+            ==> PreprocessorConstant (mkConstantMacroDef "'foo_bar" "baz")
           , testCase "backtick at the start" $
-              "#define `foo_bar baz"
-              ==> PreprocessorConstant (mkConstantMacroDef "`foo_bar" "baz")
+            """
+            #define `foo_bar baz
+            """
+            ==> PreprocessorConstant (mkConstantMacroDef "`foo_bar" "baz")
           ]
       ]
   , testGroup "Macro functions"
       [ testGroup "Single argument"
           [ testCase "Vanilla define" $
-              "#define FOO(x) x"
-              ==> PreprocessorFunction (mkFunctionMacroDef "FOO" ["x"] "x")
+            """
+            #define FOO(x) x
+            """
+            ==> PreprocessorFunction (mkFunctionMacroDef "FOO" ["x"] "x")
           , testCase "Define with some spaces" $
-              "# define FOO( x ) x"
-              ==> PreprocessorFunction (mkFunctionMacroDef "FOO" ["x"] "x")
+            """
+            # define FOO( x ) x
+            """
+            ==> PreprocessorFunction (mkFunctionMacroDef "FOO" ["x"] "x")
           ]
       , testGroup "Two arguments"
           [ testCase "Vanilla define" $
-              "#define FOO(x, y) (x < y)"
-              ==> PreprocessorFunction (mkFunctionMacroDef "FOO" ["x", "y"] "(x < y)")
+            """
+            #define FOO(x, y) (x < y)
+            """
+            ==> PreprocessorFunction (mkFunctionMacroDef "FOO" ["x", "y"] "(x < y)")
           , testCase "Define with with some spaces" $
-              "# define FOO( x , y ) ( x < y )"
-              ==> PreprocessorFunction (mkFunctionMacroDef "FOO" ["x", "y"] "( x < y )")
+            """
+            # define FOO( x , y ) ( x < y )
+            """
+            ==> PreprocessorFunction (mkFunctionMacroDef "FOO" ["x", "y"] "( x < y )")
           , testCase "Define with lots of continuation lines" $
-              "# \\\n\
-              \ define \\\n\
-              \ FOO( \\\n\
-              \ x \\\n\
-              \ , \\\n\
-              \ y \\\n\
-              \ ) \\\n\
-              \ ( x < \\\n\
-              \ y )"
-              ==> PreprocessorFunction (mkFunctionMacroDef "FOO" ["x", "y"] "( x <  y )")
-      , testCase "Define with name split by continuation line" $
-          "#define FO\\\n\
-          \O(x \\\n\
-          \ , \\\n\
-          \ y) \\\n\
-          \                                (y -\\\n\
-          \x)"
-          ==> PreprocessorFunction (mkFunctionMacroDef "FOO" ["x", "y"] "(y -x)")
+            """
+            # \\
+             define \\
+             FOO( \\
+             x \\
+             , \\
+             y \\
+             ) \\
+             ( x < \\
+             y )
+            """
+            ==> PreprocessorFunction (mkFunctionMacroDef "FOO" ["x", "y"] "( x <  y )")
+          , testCase "Define with name split by continuation line" $
+            """
+            #define FO\\
+            O(x \\
+             , \\
+             y) \\
+                                            (y -\\
+            x)
+            """
+            ==> PreprocessorFunction (mkFunctionMacroDef "FOO" ["x", "y"] "(y -x)")
           ]
       ]
   ]
@@ -120,21 +149,24 @@ defineTests = testGroup "#define"
     (==>) = makeAssertion' parsePreprocessorDefine
     (!=>) = makeAssertion (left (PP.renderString . errorMessageBody) . parsePreprocessorDefine)
 
-
 undefTests :: TestTree
 undefTests = testGroup "#undef"
   [ testCase "Vanilla undef" $
-      "#undef FOO"
-      ==> mkMacroName "FOO"
+    "#undef FOO"
+    ==> mkMacroName "FOO"
   , testCase "Undef with some spaces" $
-      "#   undef    FOO"
-      ==> mkMacroName "FOO"
+    """
+    #   undef    FOO
+    """
+    ==> mkMacroName "FOO"
   , testCase "Undef with lots of continuation lines" $
-      "# \\\n\
-      \  undef  \\\n\
-      \  F\\\n\
-      \OO"
-      ==> mkMacroName "FOO"
+    """
+    # \\
+      undef  \\
+      F\\
+    OO
+    """
+    ==> mkMacroName "FOO"
   ]
   where
     (==>) = makeAssertion' parsePreprocessorUndef
