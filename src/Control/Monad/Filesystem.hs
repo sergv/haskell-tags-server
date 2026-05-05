c4071f755c299fa4cd7649ea600da56c92e31cf7
Sergey Vinokurov <serg.foo@gmail.com>
2026-05-05 23:08:52 +0100

    Remove now-unused modules and definitions

--- src/Control/Monad/Filesystem.hs
+++ src/Control/Monad/Filesystem.hs
@@ -16,8 +16,6 @@
 module Control.Monad.Filesystem
   ( MonadFS(..)
   , SearchCfg(..)
-  , versionControlDirs
-  , defaultIgnoredGlobs
   ) where
 
 import Prelude hiding (readFile)
@@ -27,17 +25,12 @@ import Control.Monad.Catch
 import Control.Monad.Reader
 import Control.Monad.Trans.Control
 import Data.ByteString qualified as BS
-import Data.Map.Strict (Map)
-import Data.Map.Strict qualified as M
 import Data.Set (Set)
-import Data.Set qualified as S
 import Data.Text (Text)
 import Data.Text qualified as T
 import GHC.Generics (Generically(..))
 import Prettyprinter.Ext
 
-import Data.CompiledRegex
-import Data.Filesystem
 import Data.Path (FullPath, FileType(..), BaseName)
 import Data.Path qualified as Path
 
@@ -62,48 +55,19 @@ class Monad m => MonadFS m where
   readFile             :: FullPath 'File -> m BS.ByteString
   doesFileExist        :: FullPath 'File -> m Bool
   doesDirectoryExist   :: FullPath 'Dir  -> m Bool
-  findRec
-    :: (Ord k, Semigroup v)
-    => SearchCfg
-    -> CompiledRegex
-    -> (FullPath 'File -> m (Maybe (k, v)))
-    -> (FullPath 'Dir -> m (Maybe (k, v)))
-    -> m (Map k v)
 
 instance {-# OVERLAPS #-} (Monad m, MonadBaseControl IO m, MonadMask m) => MonadFS m where
   {-# INLINE readFile             #-}
   {-# INLINE doesFileExist        #-}
   {-# INLINE doesDirectoryExist   #-}
-  {-# INLINE findRec              #-}
   readFile             = liftBase . BS.readFile . T.unpack . Path.unFullPath
   doesFileExist        = Path.doesFileExist
   doesDirectoryExist   = Path.doesDirectoryExist
-  findRec SearchCfg{scShallowPaths, scRecursivePaths, scIgnoredDirs} ignoredGlobsRE =
-    findRecurCollect scIgnoredDirs ignoredGlobsRE scShallowPaths scRecursivePaths M.empty
 
 instance MonadFS m => MonadFS (ReaderT r m) where
   {-# INLINE readFile             #-}
   {-# INLINE doesFileExist        #-}
   {-# INLINE doesDirectoryExist   #-}
-  {-# INLINE findRec              #-}
   readFile             = lift . readFile
   doesFileExist        = lift . doesFileExist
   doesDirectoryExist   = lift . doesDirectoryExist
-  findRec cfg re f g   = do
-    env <- ask
-    lift $ findRec cfg re ((`runReaderT` env) . f) ((`runReaderT` env) . g)
-
-versionControlDirs :: Set (BaseName 'Dir)
-versionControlDirs = S.fromList
-  [ ".git"
-  , "_darcs"
-  , ".hg"
-  , ".svn"
-  ]
-
-defaultIgnoredGlobs :: Set Text
-defaultIgnoredGlobs = S.fromList
-  [ "*/.stack-work*/intero/*"
-  , "*/*stack-work*/intero/*"
-  , "*/flycheck_*"
-  ]
